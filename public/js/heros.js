a1d2f536bf20b4b0e044c6d07154552ff1d157e8
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-05-11 11:49:41 +0200

    Heros, mission du jour

--- public/js/heros.js
+++ public/js/heros.js
@@ -18,7 +18,7 @@ class Hero{
 class Guerrier extends Hero{
     constructor(nom, hp, attaque, rage) {
         super(nom, hp, attaque);
-        this.rage = rage;
+        this.rage = 0;
     }
 }
 class Mage extends Hero{
@@ -27,6 +27,7 @@ class Mage extends Hero{
         this.mana = mana;
     }
 }
+
 class Archer extends Hero{
     constructor(nom, hp, attaque, fleches) {
         super(nom, hp, attaque);
@@ -37,45 +38,10 @@ class Archer extends Hero{
     }
 }
 
-let garen = new Guerrier ("Garen", 400, 50,0)
-let lux = new Mage ("Lux", 200, 150)
-let ashe = new Archer ("Ashe", 300, 100)
+// Déclarations héros du jour
+// let garen = new Guerrier ("Garen", 400, 50,0)
+// let lux = new Mage ("Lux", 200, 150)
+// let ashe = new Archer ("Ashe", 300, 100)
 
-class Hero{
-    constructor(nom, hp, attaque) {
-        this.nom = nom;
-        this.hp = hp;
-        this.attaque = attaque;
-    }
-    defendre() {
-        this.attaque *= 0.5;
-        this.hp *= 2.5;
-        // +augmente les chances d'être attaqué par le boss
-    };
-    attaquer() {
-        this.attaque *= 1.4;
-        this.hp *= 0.75;
-    }
-}
-
-class Guerrier extends Hero{
-    constructor(nom, hp, attaque, rage) {
-        super(nom, hp, attaque);
-        this.rage = rage;
-    }
-}
-class Mage extends Hero{
-    constructor(nom, hp, attaque, mana) {
-        super(nom, hp, attaque);
-        this.mana = mana;
-    }
-}
-class Archer extends Hero{
-    constructor(nom, hp, attaque, fleches) {
-        super(nom, hp, attaque);
-        this.fleches = fleches;
-    }
-    nbrefleches() {
-
-    }
-}
+import {bossATuer} from './boss.js'
+console.log(`Mais de l'autre notre côté, nous avons 3 champions tous excités de battre la TERREUR de Molengeek (${bossATuer.nom})`)
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,5 @@
+// Import de boss
 import * as terreur from './boss.js'
+
+// Import de héro
+import * as heros from  './heros.js'
