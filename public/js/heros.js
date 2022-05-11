f0247031a7b37750530a0f7d4186bf1fe3d2abea
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-05-11 14:30:36 +0200

    Heros, distribution ATK

--- public/js/heros.js
+++ public/js/heros.js
@@ -65,17 +65,36 @@ console.log(`Je vous présente notre fabuleuse équipe: le Guerrier ${guerrier.n
 console.log(`Avant de lancer les hostilités, le public doit distribuer ses faveurs à chacun des Héros. Vous avez 500HP et 500ATK à répartir entre chaque Héros. VOTEZ.`)
 
 // Distribution HP
+// Pour le guerrier
 guerrier.hp = +prompt(`HP pour votre guerrier. Entre un chiffre en 100 et 300.`);
 while (guerrier.hp < 100 || guerrier.hp > 300) {
     guerrier.hp = +prompt("Attention, vous devez lui donner des HP entre 100 et 300 afin que les autres puissent taffer aussi !")
 } 
 
 let r = 500 - guerrier.hp
+// Pour le mage
 mage.hp = +prompt(`HP pour votre mage. Il vous reste ${r}points à distribuer. (Le reste sera donné automatiquement à l'archer.)`)
-while(mage.hp == r || mage.hp < 100) {
+while(mage.hp >= r || mage.hp < 100) {
     mage.hp = +prompt(`Attention, l'archer doit au moins avoir 100HP. (Il vous reste ${r}points)`)
 }
+// Pour l'archer
 archer.hp = r - mage.hp
 
 
 // Distribution ATK
+// Pour le guerrier
+guerrier.atk = +prompt(`ATK pour votre guerrier. Entre un chiffre en 100 et 300.`);
+while (guerrier.atk < 100 || guerrier.atk > 300) {
+    guerrier.atk = +prompt("Attention, vous devez lui donner des HP entre 100 et 300 afin que les autres puissent taffer aussi !")
+} 
+
+let r2 = 500 - guerrier.atk
+// Pour le mage
+mage.atk = +prompt(`ATK pour votre mage. Il vous reste ${r2}points à distribuer. (Le reste sera donné automatiquement à l'archer.)`)
+while(mage.atk >= r2 || mage.atk < 100) {
+    mage.atk = +prompt(`Donnez un chiffre entre 100 et ${r2-100}, car l'archer doit au moins avoir 100ATK.`)
+}
+// Pour l'archer
+archer.atk = r2 - mage.atk
+
+console.log(`Quelle équipe ! Voici le récap de nos héros : ${guerrier}, ${mage}, ${archer}. LE COMBAT PEUT COMMENCER.`)
